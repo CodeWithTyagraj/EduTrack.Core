ae9981a34e056ac6e2599e2a38e2cc50b7507544
Tyagraj <tyagraj.suryawanshi@globant.cmo>
2025-07-12 19:59:03 +0530

    Added Persistence folder and updated project files

---
+++ EduTrack.Core.Infrastructure/Persistence/EduTrackDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EduTrack.Domain.Entities; // Domain project ke entities ke liye reference
+
+namespace EduTrack.Infrastructure.Persistence
+{
+    public class EduTrackDbContext : DbContext
+    {
+        public EduTrackDbContext(DbContextOptions<EduTrackDbContext> options) : base(options)
+        {
+        }
+    
+
+        // Yahan apni entities ke DbSet add karo
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+
+        // Agar aur entities hain to yahan add kar sakte ho
+    }
+}
